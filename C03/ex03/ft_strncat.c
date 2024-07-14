9b406ffc8c48295abcc647dbcd74b7dffd7bf474
TaukTauk <tauktauk51833@gmail.com>
2024-07-14 12:09:48 +0700

    C03 ex03

---
+++ C03/ex03/ft_strncat.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ubuntu <ubuntu@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/14 11:45:22 by ubuntu            #+#    #+#             */
+/*   Updated: 2024/07/14 12:06:29 by ubuntu           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb);
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int	i;
+	unsigned int	n;
+
+	i = 0;
+	n = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (n < nb && src[n] != '\0')
+	{
+		dest[i + n] = src[n];
+		n++;
+	}
+	dest[i + n] = '\0';
+	return (dest);
+}
